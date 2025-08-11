8b86714cc0e64b511f8df0b683f5f85939bd6750
Freitas-2009 <luisfernando.cfreitas@gmail.com>
2025-08-11 14:25:40 -0300

    Add files via upload

---
+++ IMC.py
@@ -0,0 +1,8 @@
+print ("SEU IMC")
+
+Nome = input("Digite seu nome")
+Peso = float(input ("Digite seu peso:"))
+ALtura = float(input ("Digite sua altura"))
+IMC = Peso / (ALtura * ALtura)
+
+print (f" {Nome} seu IMC é: {IMC}")
