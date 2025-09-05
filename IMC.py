7c5ccd272a703f604b626429daad9c82ed126410
Freitas-2009 <luisfernando.cfreitas@gmail.com>
2025-09-05 13:15:54 -0300

    Update IMC.py

--- IMC.py
+++ IMC.py
@@ -1,17 +1,28 @@
 print ("SEU IMC")
 
-Nome = input("Digite seu nome")
-Peso = float(input ("Digite seu peso:"))
-ALtura = float(input ("Digite sua altura"))
+Nome = input("Digite seu nome: ")
+Peso = float(input ("Digite seu peso: "))
+ALtura = float(input ("Digite sua altura: "))
 IMC = Peso / (ALtura * ALtura)
 
 print("--------------------------------------")
-print (f" {Nome} seu IMC é: {IMC}")
+print (f"{Nome} seu IMC é: {IMC}")
 
 if IMC < 18.5:
      print("ABAIXO DO PESO")
 elif IMC < 24.9:
     print("NORMAL")
+elif IMC < 29.9:
+    print("SOBREPESO")
+elif IMC < 34.9:
+    print("OBESIDADE GRAU 1")
+elif IMC < 39.9:
+    print("OBESIDADE GRAU 2")
 else:
-    print("OBESIDADE")
+    print("OBESIDADE GRAU 3")
+
+if IMC >= 30.0:
+    print("❗CUIDADO COM A SAÚDE❗")
+else:
+    print("TUDO OK ✅")
 print("--------------------------------------")
